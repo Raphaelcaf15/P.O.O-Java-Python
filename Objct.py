48b481039b87345ee01c4086e0d7d7511b370d1a
Raphaelcaf15 <124456315+Raphaelcaf15@users.noreply.github.com>
2024-04-10 16:07:48 -0300

    Update Objct.py

--- Objct.py
+++ Objct.py
@@ -1,5 +1,5 @@
-from Classes import Pessoa as ps
+# Oi penis vagina pepeca
+from Class import Pessoa as ps
 import datetime as dt
-person = ps("10-06-1960")
-print(person.Saudacao())
-# penis vagina pepeca
+person = ps("Olavio", "10-06-1960", "Olaf", )
+print(person.saudacao())
